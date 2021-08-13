e1a8b25a5d275c6b5018ba3da67d2ed5b7b70d55
xiaochao.li <1>
2021-08-13 10:14:32 +0800

    Initial commit

---
+++ tUtils/src/main/java/com/lixiaozhao/tutils/Bean.java
@@ -0,0 +1,19 @@
+package com.lixiaozhao.tutils;
+
+/**
+ * @ClassName Bean
+ * @Description TODO
+ * @Author lixiaozhao
+ * @Date 2021/8/13 9:58
+ * @Version 1.0
+ */
+public class Bean {
+    public Bean(String info) {
+        this.info = info;
+    }
+
+    public Bean() {
+    }
+
+    public String info;
+}
---
+++ tUtils/src/main/java/com/lixiaozhao/tutils/T.java
@@ -0,0 +1,22 @@
+package com.lixiaozhao.tutils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * @ClassName T
+ * @Description TODO
+ * @Author lixiaozhao
+ * @Date 2021/8/13 9:57
+ * @Version 1.0
+ */
+public class T {
+    public void show(Context context) {
+        Gson gson = new GsonBuilder().create();
+        String s = gson.toJson(new Bean("Info"));
+        Toast.makeText(context, s, Toast.LENGTH_LONG).show();
+    }
+}
